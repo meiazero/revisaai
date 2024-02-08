a1296b9750d2b546d2fa42c0f9a4acb8b9ea6ba0
Emanuel Avila <avilaemanueel@gmail.com>
2024-02-07 22:04:39 -0300

    refactor `Header` component

--- components/header.tsx
+++ components/header.tsx
@@ -1,47 +1,25 @@
-import { BadgeDollarSign, Clock2, Info } from "lucide-react";
+import { userDropdownItems, userListItems } from "@/config/userMenuConfig";
 import Link from "next/link";
 import { Icons } from "./icons";
-import { DropDownItems, NavMenuDropDown } from "./nav-menu";
+import { NavMenuDropDown } from "./nav-menu-dropdown";
+import NavMenuList from "./nav-menu-list";
 import { WaitingListPopup } from "./waiting-list-popup";
-
-export const normalUserItems: Array<DropDownItems> = [
-	{
-		title: "Menu",
-		items: [
-			{
-				title: "Sobre",
-				href: "/about",
-				icon: Info,
-				isActive: true,
-			},
-			{
-				title: "Preços",
-				href: "/prices",
-				icon: BadgeDollarSign,
-				isActive: true,
-			},
-			{
-				title: "Lista de espera",
-				href: "/waiting-list",
-				icon: Clock2,
-				isActive: true,
-			},
-		],
-	},
-];
 
 export function Header() {
 	return (
 		<header className="h-16 w-full border-b border-zinc-200 bg-white">
 			<div className="flex h-full flex-row items-center justify-between px-4 md:container">
-				<Link href="/" title="Página inicial">
-					<Icons.logo className="h-8 w-auto" />
-				</Link>
+				<div className="flex flex-row items-center gap-24">
+					<Link href="/" title="Página inicial">
+						<Icons.logo className="h-8 w-auto" />
+					</Link>
+					<NavMenuList listItems={userListItems} />
+				</div>
 
 				<div className="flex flex-row items-center gap-4 md:gap-8">
 					{/* Button cta */}
 					<WaitingListPopup className="hidden md:flex" />
-					<NavMenuDropDown dropDownItems={normalUserItems} />
+					<NavMenuDropDown dropDownItems={userDropdownItems} />
 				</div>
 			</div>
 		</header>
