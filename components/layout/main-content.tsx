7eaa6b45b69bab566747c3550fea7e88f4f297e6
Emanuel Avila <avilaemanueel@gmail.com>
2024-02-07 22:03:19 -0300

    add `MainContent` component

---
+++ components/layout/main-content.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+interface MainContentProps {
+	className?: string;
+	children: React.ReactNode;
+}
+
+export default function MainContent({ children, className }: MainContentProps) {
+	return (
+		<main className={cn("container flex min-h-[calc(100vh-10rem)]", className)}>
+			{children}
+		</main>
+	);
+}
