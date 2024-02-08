15f33aaac156c3fa90a21ff3451b350bb7013ae2
Emanuel Avila <avilaemanueel@gmail.com>
2024-02-07 22:06:29 -0300

    initial structure to `app` page

---
+++ app/app/page.tsx
@@ -0,0 +1,10 @@
+import ApikeyButton from "@/components/api-key-button";
+import MainContent from "@/components/layout/main-content";
+
+export default function Page() {
+	return (
+		<MainContent>
+			<ApikeyButton />
+		</MainContent>
+	);
+}
