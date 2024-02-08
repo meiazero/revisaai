f4077dbe4988c3a82cff3a84eca401a378587cd9
Emanuel Avila <avilaemanueel@gmail.com>
2024-02-07 22:04:16 -0300

    add `NavMenuList` component

---
+++ components/nav-menu-list.tsx
@@ -0,0 +1,27 @@
+import { NavItem } from "@/types";
+import Link from "next/link";
+
+export interface Listitems extends NavItem {}
+
+interface NavMenuListProps {
+	listItems: Array<Listitems>;
+}
+
+export default function NavMenuList({ listItems }: NavMenuListProps) {
+	return (
+		<div className="hidden md:block">
+			<ul className="flex flex-row gap-4  ">
+				{listItems.map(({ href, title, isActive }: NavItem) => (
+					<li key={title}>
+						<Link
+							href={href}
+							className="font-semibold lowercase hover:underline"
+						>
+							{title}
+						</Link>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+}
