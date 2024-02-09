b2338f46660cac84eafee6822494dd041124eb58
Emanuel Avila <avilaemanueel@gmail.com>
2024-02-09 00:15:46 -0300

    chore: rename component

--- components/layout/main-container.tsx
+++ components/layout/main-container.tsx
@@ -1,12 +1,12 @@
 import { cn } from "@/lib/utils";
 import React from "react";
 
-interface MainContentProps {
+interface MainContainerProps {
 	className?: string;
 	children: React.ReactNode;
 }
 
-export function MainContent({ children, className }: MainContentProps) {
+export function MainContainer({ children, className }: MainContainerProps) {
 	return (
 		<main className={cn("container flex min-h-[calc(100vh-10rem)]", className)}>
 			{children}
