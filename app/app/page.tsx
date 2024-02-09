556b908c91b59be6eeda391d57fa931af40252fc
Emanuel Avila <avilaemanueel@gmail.com>
2024-02-09 00:17:01 -0300

    refactor: `app` page

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,10 +1,9 @@
-import { ApikeyButton } from "@/components/api-key-button";
-import { MainContent } from "@/components/layout/main-content";
+import { MainContainer } from "@/components/layout/main-container";
 
 export default function Page() {
 	return (
-		<MainContent>
-			<ApikeyButton />
-		</MainContent>
+		<MainContainer className="flex-col gap-4 py-8 ">
+			<p>Loading content</p>
+		</MainContainer>
 	);
 }
